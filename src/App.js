c6ef6e67a45e9df95ce7353ae676fef2b44e8b82
mamadou sylla <m27sylla@gmail.com>
2020-08-06 19:20:34 +0200

    test de la searchbar ok

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Search from './components/Search';
 
 
 function App()
 {
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {}
+  });
+  const apiUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=72ff8aa9";
+  const handleInput = (e) =>
+  {
+    let s = e.target.value;
+    setState(prevState =>
+    {
+      return { ...prevState, s: s }
+    })
+    console.log(state.s);
+  }
   return (
     <div className="App">
       <header>
         <h1>Movie database</h1>
       </header>
       <main>
-        <Search />
+        <Search handleInput={handleInput}/>
       </main>
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-function Search() {
-	return (
-		<section className="searchbox-wrap">
-	 <input type="text" placeholder="Find whatever you want" className="searchbox"/>
-		</section>
-	)
+function Search({ handleInput })
+{
+  return (
+    <section className="searchbox-wrap">
+      <input 
+      type="text"
+      placeholder="Find whatever you want" 
+      className="searchbox" 
+      onChange={handleInput} 
+      />
+    </section>
+  )
 }
 
 export default Search
