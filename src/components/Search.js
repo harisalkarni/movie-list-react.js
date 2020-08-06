ebbe0b1a97ceff9fae5c2ed9c1da2e731c7f41a3
mamadou sylla <m27sylla@gmail.com>
2020-08-06 13:00:58 +0200

    design searchbar et création component

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Search() {
+	return (
+		<section className="searchbox-wrap">
+	 <input type="text" placeholder="Find whatever you want" className="searchbox"/>
+		</section>
+	)
+}
+
+export default Search
