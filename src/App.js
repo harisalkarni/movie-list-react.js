4c04ed3e7779b81ef3ee6cee1877c7df5efbbaa5
mamadou sylla <m27sylla@gmail.com>
2020-08-10 00:00:29 +0200

    popup terminée css à faire

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Search from './components/Search';
 import Results from './components/Results';
 import axios from 'axios';
+import Popup from './components/Popup';
 
 
 function App()
@@ -12,7 +13,7 @@ function App()
     selected: {}
   });
 
-  const apiUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=72ff8aa9";
+  const apiUrl = "http://www.omdbapi.com/?apikey=72ff8aa9";
 
 
 
@@ -20,39 +21,67 @@ function App()
   {
     if (e.key === "Enter")
     {
-      axios(apiUrl + "&s=" + state.s).then(({data}) => {
+      axios(apiUrl + "&s=" + state.s).then(({ data }) =>
+      {
         let results = data.Search;
         console.log(data);
-        setState(prevState => {
-    return { ...prevState, results: results }
-  })
-});
+        setState(prevState =>
+        {
+          return { ...prevState, results: results }
+        })
+      });
     }
   }
 
 
 
-const handleInput = (e) =>
-{
-  let s = e.target.value;
-  setState(prevState =>
+  const handleInput = (e) =>
   {
-    return { ...prevState, s: s }
-  })
-  console.log(state.s);
-}
-return (
-  <div className="App">
-    <header>
-      <h1>Movie database</h1>
-    </header>
-    <main>
-      <Search handleInput={handleInput} search={search} />
-      <Results results={state.results} />
+    let s = e.target.value;
+    setState(prevState =>
+    {
+      return { ...prevState, s: s }
+    });
+  }
 
-    </main>
-  </div>
-);
+  //Variable pour la popup quand on clique sur l'image du film
+  const openPopup = id =>
+  {
+    axios(apiUrl + "&i=" + id).then(({ data }) =>
+    {
+      let result = data;
+      console.log(result);
+      setState(prevState =>
+      {
+        return { ...prevState, selected: result }
+      });
+    });
+  }
+
+  //fin de la variable
+
+  const closePopup = () =>
+  {
+    setState(prevState =>
+    {
+      return { ...prevState, selected: {} }
+    });
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Movie database</h1>
+      </header>
+      <main>
+        <Search handleInput={handleInput} search={search} />
+        <Results results={state.results} openPopup={openPopup} />
+
+        {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup={closePopup} /> :false}
+
+      </main>
+    </div>
+  );
 }
 
 export default App;
